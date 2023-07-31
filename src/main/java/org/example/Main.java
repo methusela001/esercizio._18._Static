bf924e5226ef318dec1962bb70529cb0830dd039
Methusela Fernando <136374272+methusela001@users.noreply.github.com>
2023-07-31 18:56:56 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,16 @@
 package org.example;
 
-public class mn {
+class Main {
+    static void main(String[] args) {
+        Employee employee1 = new Employee("Andrea", "Rossi", "Via Roma");
+        Employee employee2 = new Employee("Mario", "Rossi", "Via Roma");
+
+        Badge badge1 = new Badge();
+        badge1.employee = employee1;
+        badge1.showBadgeDetails();
+
+        Badge badge2 = new Badge();
+        badge2.employee = employee2;
+        badge2.showBadgeDetails();
+    }
 }
